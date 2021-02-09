0de0ebe009a192883cfc8f3b1488e8c6ccaccbdf
Antonio Carlos Catani neto <cataninetoo@gmail.com>
2021-02-08 23:20:40 -0300

    Melhorias nos testes

--- Teste.js
+++ Teste.js
@@ -1,36 +1,64 @@
 const puppeteer = require('puppeteer');
-let scrape = async () => {
+let scrape = async (pageNumber) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    let hasResults = true;
-    let i = 1;
-    while (hasResults) {
-        await page.goto('https://www.kabum.com.br/hardware/placa-de-video-vga?pagina=' + i + '&ordem=3&limite=100&prime=false&marcas=[]&tipo_produto=[]&filtro=[]');
-        i++;
-        hasResults = false;
-        const query = await page.evaluate(() => {
-            const results = { nomes: [], valores: [] };
+    await page.goto('https://www.kabum.com.br/hardware/placa-de-video-vga?pagina=' + pageNumber + '&ordem=3&limite=100&prime=false&marcas=[]&tipo_produto=[]&filtro=[]');
 
-            document.querySelectorAll('a.item-nome')
-                .forEach((result) => results.nomes.push(result.innerText));
+    const query = await page.evaluate(() => {
+        const results = {
+            arrayValues: [],
+            foundUnavailable: false
+        };
 
-            document.querySelectorAll('div.qatGF')
-                .forEach((result) => results.valores.push(result.innerText));
+        document.querySelectorAll('div.jmuOAh').forEach((result) => {
+            //isAvailable = Verifica se existem classes que indicam item indisponível
+            const isAvailable = result.getElementsByClassName('jLtPVV').length === 0;
 
-            console.log(results);
-            return results;
-        })
-        browser.close()
-        return query;
-    }
+            //Se um item não está disponível, indica que é a última página de resultados
+            if (!isAvailable) {
+                results.foundUnavailable = true;
+            }
+
+            //Salva valores obtidos no HTML em variáveis para facilitar a reutilização
+            const productName = result.getElementsByClassName('item-nome')[0].innerText;
+            const productValue = result.getElementsByClassName('qatGF')[0].innerText;
+            const productValueInstallments = result.getElementsByClassName('ksiZrQ')[0].innerText
+            
+            //Se o item verificado estiver disponível salva no vetor
+            if (isAvailable) { //&&productName.includes(modeloPesquisa)
+                results.arrayValues.push({
+                    nome: productName,
+                    valorParc: productValueInstallments,
+                    valorAV: productValue
+                });
+            }
+        });
+        return results;
+    })
 
     await browser.close();
+    return query;
+
 };
 
-scrape().then((value) => {
-    for (let i = 0; i < value.nomes.length; i++) {
-        if (value.nomes[i].includes('3070')) {
-            console.log(value.nomes[i].concat(' -- Valor: ').concat(value.valores[i]));
+
+//Reformular totalmente, funciona para testes pelo menos
+const doScrape = () => {
+    scrape(i).then((value) => {
+        i++;
+
+        for (el of value.arrayValues) {
+            console.log('Nome: ' + el.nome);
+            console.log('Valor à vista: ' + el.valorAV);
+            console.log('Valor à prazo: ' + el.valorParc);
+            console.log('####################################################################################');
         }
-    }
-})
+        if (!value.foundUnavailable) {
+            doScrape(i)
+        }
+    });
+}
+
+let i = 1;
+doScrape();
+
