850258d8cd21a615e4f8026d0619b1517dc732bc
Antonio Carlos Catani neto <cataninetoo@gmail.com>
2021-02-14 01:42:46 -0300

    Changes for heroku

--- Index.js
+++ Index.js
@@ -2,13 +2,18 @@ const browserObject = require('./browser');
 const scraperController = require('./pageController');
 const createFile = require('./FileCreator');
 
-//Start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+const runTasks = () => {
+    //Start the browser and create a browser instance
+    let browserInstance = browserObject.startBrowser();
 
-// Pass the browser instance to the scraper controller
-scraperController(browserInstance).then((results) => {
-    //Create the file with the results from the scraper
-    createFile(results);
+    // Pass the browser instance to the scraper controller
+    scraperController(browserInstance).then((results) => {
+        //Create the file with the results from the scraper
+        createFile(results);
+    });
 }
-);
+runTasks();
+setInterval(() => {
+    runTasks();
+}, 5 * 60 * 1000)
 
