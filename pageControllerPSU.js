68b13398d53c10ba09002af7f5c2e845d805dff0
Antonio Carlos Catani neto <cataninetoo@gmail.com>
2021-02-16 00:28:57 -0300

    Revert "Now case is fixed"
    This reverts commit d234d1f93e502437fbcef90e967724d5a3d8209a.

--- pageControllerPSU.js
+++ pageControllerPSU.js
@@ -1,6 +1,6 @@
 const scraperKabumPSU = require('./Scrapers/PSU/ScraperKabumPSU');
 const scraperPichauPSU = require('./Scrapers/PSU/ScraperPichauPSU');
-const scraperTerabytePSU = require('./Scrapers/PSU/ScraperTerabytePSU');
+const scraperTerabytePSU = require('./Scrapers/PSU/ScraperTerabytePSUs');
 const scraperGKInfoStorePSU = require('./Scrapers/PSU/scraperGKInfoStorePSU');
 
 
