b7c9e799f20fa777101a4087df37d784c76e9f50
Antonio Carlos Catani neto <cataninetoo@gmail.com>
2021-02-18 13:20:31 -0300

    Tests with more logs on file creation

--- FileCreator.js
+++ FileCreator.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 
 async function createFile(results) {
-    console.log("Creating the file.....");
+    console.log("Call for create file");
 
     let dataForFile = JSON.stringify(results);
-    fs.writeFileSync('./public/Precos.json', dataForFile);
+
+    if (dataForFile) {
+        console.log("Creating the file.....");
+
+        fs.writeFileSync('./public/Precos.json', dataForFile);
+
+    }
 }
 
 module.exports = (JSONResults) => createFile(JSONResults);
